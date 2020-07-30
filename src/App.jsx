e75f0ec3f5cef3ccd02fb969a53eeec2b9cd9d86
munna <munna99sk@gmail.com>
2020-07-30 17:36:19 +0530

    renamed app.js to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+function App() {
+  return (
+    <div classname="react">Learn React hai react</div>
+  <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Zayn_Malik_April_2014.png/200px-Zayn_Malik_April_2014.png'><source/></img>
+  )
+};
+
+export default App;
