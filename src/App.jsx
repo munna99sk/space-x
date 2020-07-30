cc0b980134b8f09ac6d943d12e71ae3863153191
munna <munna99sk@gmail.com>
2020-07-30 18:12:30 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./style.css";
 function App() {
-  return (
-    <div classname="react">Learn React hai react</div>
-  <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Zayn_Malik_April_2014.png/200px-Zayn_Malik_April_2014.png'><source/></img>
-  )
-};
+  return <div classname="react">Learn React welcome to react</div>;
+}
 
 export default App;
